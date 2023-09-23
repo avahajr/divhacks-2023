aca56863fe70f6a5114aabcab348e705c05bab3b
Ava Hajratwala <100488972+avahajr@users.noreply.github.com>
2023-09-23 15:28:13 -0400

    add background shape

--- src/App.js
+++ src/App.js
@@ -10,7 +10,13 @@ function App() {
       {/* TODO: get props.name from the db*/}
       <CollapsibleNavbar name="Jane Barnard" />
       <Container>
+        {/* <div className="purpose">Trade your...</div> */}
         <SearchBar />
+        <h2 id="mission">Mission</h2>
+
+        <Container className="background-shape">
+          <p>Blah blah blah blah blah</p>
+        </Container>
       </Container>
     </>
   );
