23940e6ce6831199a1ecfea2bb69ab12af87ab47
Ava Hajratwala <100488972+avahajr@users.noreply.github.com>
2023-09-23 22:44:11 -0400

    centered logo

--- src/components/CollapsibleNavbar.js
+++ src/components/CollapsibleNavbar.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import { Nav, Navbar, NavDropdown, Image, Col } from "react-bootstrap";
+import { useWindowSize } from "@uidotdev/usehooks";
 
 function CollapsibleNavbar(props) {
+  let { _, width } = useWindowSize();
+  console.log(width);
   return (
     <>
       <Navbar
@@ -14,17 +17,21 @@ function CollapsibleNavbar(props) {
         <Container>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav as={Col} md={5}>
+            <Nav as={Col} md={4}>
               <Nav.Link href="#mission">Mission Statement</Nav.Link>
               <Nav.Link href="#faq">FAQ</Nav.Link>
             </Nav>
-            <Nav className="d-flex align items-center" as={Col} md={6}>
-              <Navbar.Brand href="#home">
-                <img src="logo.png" width={300} alt="logo" />
-              </Navbar.Brand>
+            <Nav className="d-flex align" as={Col} md={4}>
+              {width > 992 ? (
+                <Navbar.Brand className="mx-auto" href="#home">
+                  <img src="logo.png" width={300} alt="logo" />
+                </Navbar.Brand>
+              ) : (
+                <></>
+              )}
             </Nav>
-            <Col ms={2}></Col>
-            <Nav className="d-flex align-items-center " as={Col} md={2}>
+            {/* <Col ms={4}></Col> */}
+            <Nav className="d-flex align-items-center" as={Col} md={4}>
               <Image
                 height={25}
                 width={25}
